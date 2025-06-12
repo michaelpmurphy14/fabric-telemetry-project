8adb7a098feac9cd8b934ddaaeb202c69e38b163
michaelpmurphy14 <135272573+michaelpmurphy14@users.noreply.github.com>
2025-06-12 12:16:09 -0500

    Update secrets.h

--- esp32-firmware/include/secrets.h
+++ esp32-firmware/include/secrets.h
@@ -2,17 +2,17 @@
 #define SECRETS_H
 
 // WiFi credentials
-#define WIFI_SSID     "homewifi14"
-#define WIFI_PASSWORD "Woodhaven14!"
+#define WIFI_SSID     "SSID"
+#define WIFI_PASSWORD "Password"
 
-#define AWS_IOT_ENDPOINT "ae9hle7ve5t24-ats.iot.us-east-1.amazonaws.com"
-#define AWS_IOT_PORT 8883
+#define AWS_IOT_ENDPOINT "AWS_IOT_ENDPOINT"
+#define AWS_IOT_PORT PORT
 
 // MQTT credentials
-#define HIVEMQ_ENDPOINT   "95a200ab13474cfa9a8848426e3dec29.s1.eu.hivemq.cloud"
+#define HIVEMQ_ENDPOINT   "HIVEMQ_ENDPOINT"
 #define HIVEMQ_PORT     8883
-#define HIVEMQ_USER     "mpmurphy14"
-#define HIVEMQ_PASS     "Woodhaven14!"
+#define HIVEMQ_USER     "HIVEMQ_USER"
+#define HIVEMQ_PASS     "HIVEMQ_PASS"
 
 #endif
 
